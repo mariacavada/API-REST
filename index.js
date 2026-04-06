44583f42df2485c4f514245b958f601aa07aa1a2
API REST <a01723282@tec.mx>
2026-04-06 12:16:03 -0600

    API Basica

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 import express from "express"
+
+const app = express()
+
+app.get("/",(req,res) =>{res.send("Hola mundo desde la API")})
+
+const PORT = 8000
+
+app.listen(PORT,console.log("http://localhost:"+PORT))
